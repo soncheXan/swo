2892b6419049b118f5c433648dba2997f1f5f46b
JuanSWO <juan.airado@softwareone.com>
2022-09-14 12:15:29 +0200

    First commit

---
+++ js/001/apuntes/main.js
@@ -0,0 +1,14 @@
+//Crear una alerta
+//alert('Esta es una alerta');
+
+//Declarar una variable de ambito global
+var nombre = 'Ana';
+
+//Imprimir por consola
+console.log(nombre);
+
+//Imprimir por pantalla
+document.write("<h1>Hola</h1>");
+
+//Declarar una variable de ambito local
+let edad = 27;
---
+++ js/001/ejercicios/utils/EjercicioCinco.java
@@ -0,0 +1,10 @@
+package ejercicios.utils;
+
+public class EjercicioCinco{
+    public static boolean esPar (int numero){
+        if(numero%2 == 0)
+            return true;
+        else
+            return false;
+    }
+}
