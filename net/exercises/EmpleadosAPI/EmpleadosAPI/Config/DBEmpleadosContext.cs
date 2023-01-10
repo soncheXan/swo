0c522b9bc058f1bf16cb175577cb17c69ce565d8
juan.airado <a20juanav@iessanclemente.net>
2023-01-10 22:57:30 +0100

    FEAT: React (ejercicio1) || .Net (ejercio2)

---
+++ net/exercises/EmpleadosAPI/EmpleadosAPI/Config/DBEmpleadosContext.cs
@@ -0,0 +1,15 @@
+﻿using EmpleadosAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpleadosAPI.Config
+{
+    public class DBEmpleadosContext : DbContext
+    {
+        public DBEmpleadosContext(DbContextOptions<DBEmpleadosContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
---
+++ net/exercises/EmpleadosAPI/EmpleadosAPI/Controllers/EmpleadoController.cs
@@ -0,0 +1,30 @@
+﻿using EmpleadosAPI.Config;
+using EmpleadosAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpleadosAPI.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        private readonly DBEmpleadosContext _context;
+        public EmpleadoController(DBEmpleadosContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("list")]
+        public async Task<ActionResult<List<Empleado>>> Get()
+        {
+            return await _context.Empleados.ToListAsync();
+        }
+
+        [HttpPost("save")]
+        public async Task<ActionResult> Post(Empleado empleado)
+        {
+            _context.Add(empleado);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
